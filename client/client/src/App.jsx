2657f6256c11dbae32d1fc282f9d3244014c5deb
Pranshu verma <pv2105@srmist.edu.in>
2025-05-31 22:57:22 +0530

    Set up React frontend with Vita

---
+++ client/client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Legal Consult Platform</h1>
+      <p>This is the beginning, boss 🧱</p>
+    </div>
+  )
+}
+
+export default App
